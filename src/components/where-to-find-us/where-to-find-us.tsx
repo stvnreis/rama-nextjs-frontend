b2a4f374473eae009d8fd52d3f02b3919e482402
stvnreis <stvnreis@gmail.com>
2023-10-25 08:59:48 -0300

    Ajuste refresh token

--- src/components/where-to-find-us/where-to-find-us.tsx
+++ src/components/where-to-find-us/where-to-find-us.tsx
@@ -1,10 +1,3 @@
-import { Map } from './map';
-
 export const WhereToFindUs = () => {
-  return (
-    <div className="where-to-find-us-container">
-      Onde nos encontrar
-      <Map />
-    </div>
-  );
+  return <div className="where-to-find-us-container">Onde nos encontrar</div>;
 };
