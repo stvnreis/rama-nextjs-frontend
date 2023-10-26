e6d151c7ce39c31c0d1e899df634e13e725be0a3
stvnreis <stvnreis@gmail.com>
2023-10-26 17:42:42 -0300

    Nossos assinantes e mapa

--- src/components/where-to-find-us/map.tsx
+++ src/components/where-to-find-us/map.tsx
@@ -1,13 +1,48 @@
-// import { GoogleMap } from '@react-google-maps/api';
-// import 'leaflet/dist/leaflet.css';
+import React, { useEffect } from 'react';
 
-// import { MapContainer, TileLayer } from 'react-leaflet';
+import { Loader } from '@googlemaps/js-api-loader';
 
-// export const Map = () => {
-//   return (
-//     <GoogleMap
-//       mapContainerStyle={containerStyle}
-//       center={{ lat: position.lat, lng: position.lng }}
-//     ></GoogleMap>
-//   );
-// };
+export const Map = () => {
+  const mapRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const initMap = async () => {
+      console.log('map init');
+
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+        version: 'dayly',
+        region: 'BR',
+        language: 'pt-br',
+      });
+
+      const { Map } = await loader.importLibrary('maps');
+
+      const { Marker } = (await loader.importLibrary(
+        'marker',
+      )) as google.maps.MarkerLibrary;
+
+      const position = {
+        lat: -21.282465379446943,
+        lng: -50.32671077546191,
+      };
+
+      const mapOptions: google.maps.MapOptions = {
+        center: position,
+        zoom: 18,
+        mapId: 'RAMABUSINESS_NEXTJS_MAPID',
+      };
+
+      const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+
+      new Marker({
+        map,
+        position,
+      });
+    };
+
+    initMap();
+  }, []);
+
+  return <div className="h-full w-full rounded-lg" ref={mapRef}></div>;
+};
--- src/components/where-to-find-us/where-to-find-us.tsx
+++ src/components/where-to-find-us/where-to-find-us.tsx
@@ -1,3 +1,19 @@
+import { Map } from './map';
+
 export const WhereToFindUs = () => {
-  return <div className="where-to-find-us-container">Onde nos encontrar</div>;
+  return (
+    <div className="where-to-find-us-container">
+      <h2 className="text-2xl font-semibold">Venha nos visitar!</h2>
+      <div className="w-full h-96 flex flex-col lg:flex-row px-10 md:px-20 gap-5">
+        {/* <div className="w-full flex justify-start items-start">
+          <span className="text-justify">
+            Construído em Iocalização privilegiada, próximo ao Poupa-Tempo e
+            Câmara Municipal, Ramá Business está localizado no novo centro da
+            cidade de Birigui-SP.
+          </span>
+        </div> */}
+        <Map />
+      </div>
+    </div>
+  );
 };
