a97425a3cef2b8ef794f13e728b63ad619827514
Steven Reis <88798335+stvnreis@users.noreply.github.com>
2023-11-09 17:26:28 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ['image/webp', 'image/avif'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'drive.google.com',
       },
     ],
-    domains: ['drive.google.com'],
   },
 };
 
