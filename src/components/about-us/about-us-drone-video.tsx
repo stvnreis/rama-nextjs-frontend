2842bf533a1abd94abb690bb3b11d943d8727a18
Steven Reis <stvnreis@gmail.com>
2024-01-11 19:46:12 -0300

    Atualização do Vídeo

--- src/components/about-us/about-us-drone-video.tsx
+++ src/components/about-us/about-us-drone-video.tsx
@@ -1,12 +1,15 @@
+import YouTube from 'react-youtube';
+
 export const AboutUsDroneVideo = () => {
   return (
-    <video
-      src="https://drive.google.com/uc?export=view&id=1d1L5meYUX0EKsDJN6trgjmiz1RgtpQ-i"
-      height="40vh"
-      width="40vw"
-      controls={true}
-      className="overflow-hidden w-full h-full"
-      style={{ objectFit: 'cover' }}
+    <YouTube
+      videoId="wVtuddc4_Cw"
+      opts={{ playerVars: { autoPlay: 0 } }}
+      onReady={_onReady}
     />
   );
 };
+
+function _onReady(event: any) {
+  event.target.pauseVideo();
+}
